d9c9578c036514b56a2e9392974869bfa10b8f92
zxpron916-dot <zxpron916@gmail.com>
2026-06-28 20:02:42 +0530

    Miu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 subprocess.Popen(["python", "Groupscanbot.py"])
-subprocess.Popen(["python", "Mainscan.py"])
+subprocess.Popen(["python", "Mainscan (1).py"])
 
 while True:
     time.sleep(60)
